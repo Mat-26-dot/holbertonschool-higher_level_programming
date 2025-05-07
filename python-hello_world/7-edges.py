f09e108ff9899db8a5fb2b847a24cfd0ea50a6b3
md <md@outlook.com>
2025-05-07 13:26:51 +1000

    7-edges.py

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word = (word[0:3])
+word = (word[:-2])
+word = (word[1:-1])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
