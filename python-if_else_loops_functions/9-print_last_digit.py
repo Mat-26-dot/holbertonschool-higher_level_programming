b2cddf3bafc058c97f2da622da508078837f1c5f
md <md@outlook.com>
2025-05-09 14:24:46 +1000

    9-print_last_digit.py

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+
+    last_digit = abs(number) % 10
+    print(last_digit)
+    return last_digit
+result = print_last_digit(125)
+print("Returned:", result)
