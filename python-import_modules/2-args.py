06d859f592fa3c409b7295a7b06c2ee1baeda35d
mat <md@outlook.com>
2025-05-10 06:13:05 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,10 +6,10 @@ if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     
     if num_args == 0:
-        print(f"Number of argument(s).")
+        print("0 arguments")
 
     elif num_args == 1:
-        print(f"Number of argument(s): 1 argument:")
+        print("1 argument:")
 
     else:
-        print(f"Number of argument(s): {num_args} arguments:")
+        print(f"{num_args} arguments:")
