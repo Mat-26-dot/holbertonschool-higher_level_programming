ebdf4cd5f9dc5fb23acf7aa62cfc9c67d105fbfc
md <md@outlook.com>
2025-05-12 01:43:11 +0000

    5-variable_load.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,8 +2,9 @@
 import sys
 
 if __name__ == "__main__":
-
-    args = sys.argv[1:]
-int_args = map(int, args)
-result = sum(map(int, args))
-print(result)
+    
+    args = sys.argv[1:]      # Get all command-line arguments except the script name
+    
+    int_args = map(int, args)  # Convert each argument to an integer
+    result = sum(int_args)     # Sum all the integers
+    print(result)              # Print the resulti
