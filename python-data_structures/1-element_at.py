ca1f1724a1ca56eab18429f4e59fb3d9db0e72b3
md <md@outlook.com>
2025-05-12 16:09:21 +1000

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
 
-    if idx <0: #if idx is negative return None
+    if idx <0:
         return None
 
-    if idx >= len(my_list): #if idx is the length of my_list return None
+    if idx >= len(my_list):
         return None
  
-    return my_list[idx] #if idx is valid return the element
+    return my_list[idx]
