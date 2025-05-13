ef923a322f261226f034fd3944fd2797c901d0f5
md <md@outlook.com>
2025-05-13 11:01:32 +1000

    2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
 
-    if idx < 0:    #Check idx if is negative 
+    if idx < 0:    # Check idx if is negative
         return my_list
 
-    if idx >= len(my_list):    #Check if idx is out of range
+    if idx >= len(my_list):    # Check if idx is out of range
         return my_list
 
     my_list[idx] = element
