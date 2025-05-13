12833e21a134130a8f93c80d90ed5e6d107f30f8
md <md@outlook.com>
2025-05-13 10:56:01 +1000

    2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -4,10 +4,9 @@ def replace_in_list(my_list, idx, element):
     if idx < 0: #Check idx if is negative 
         return my_list
 
-    if idx>= 5 (my_list): #Check if idx is out of range
+    if idx>= len(my_list): #Check if idx is out of range
         return my_list
     
-    else:
-        my_list[idx] = element
+    my_list[idx] = element
 
     return my_list
