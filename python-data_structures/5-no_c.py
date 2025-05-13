d431335089832c7a83f4042f0b5ad9154f0561c5
md <md@outlook.com>
2025-05-13 14:50:03 +1000

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    
+
     result = []    # Step 2: Create an empty list to hold charcters
 
     for char in my_string:    # Step 3: Go through each character in string
-        
-        if char != 'c' and char != 'C': # Step 4: If the char is not 'c' or 'C' 
+
+        if char != 'c' and char != 'C':    # Step 4: If the char is not 'c' or 'C' 
 
             result.append(char)    # append characters in new_string
     new_string = ''.join(result)   # Step 5: Combine the list into a new_string
