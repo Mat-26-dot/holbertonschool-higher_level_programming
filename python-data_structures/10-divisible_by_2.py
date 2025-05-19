11dc08c2f2eb8e79871080e4a990348b7a3612e3
md <md@outlook.com>
2025-05-19 13:41:02 +1000

    10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -7,7 +7,7 @@ def divisible_by_2(my_list=[]):
 
         if my_list[i] % 2 == 0:
             result.append(True)
-    else:
-        result.append(False)
+        else:
+            result.append(False)
     
         return result
