c4101b80f2cd06d87aa8f14a5ee3d65fa9fec408
md <md@outlook.com>
2025-05-19 18:11:57 +1000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
 # Use map to square each element in each row, then convert to list
-    return (list(map(lambda x: x **2, row)) for row in matrix)
+    return [list(map(lambda x: x **2, row)) for row in matrix]
