842082d2cab210fda3478fcfa36234fe07c50e91
md <md@outlook.com>
2025-05-19 12:16:37 +1000

    9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
 
-    if my_list == "":
+    if not my_list:
         return None
+    current_max = my_list[0]
     for num in my_list:
-        current_max = 90
+        if num > current_max:
+            current_max = num
     return current_max
