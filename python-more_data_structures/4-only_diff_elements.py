17e695575f4e590d3701cc802dd462ceeb998c87
md <md@outlook.com>
2025-05-20 13:36:06 +1000

    5-number_keys.py

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-
-    result = set()
-    for elements in set_1:
-        if elements not in set_2:
+    
+    result = set()  # Create an empty set to hold the result
+    
+    for elements in set_1:  # Loop through elements in set_1
+        if elements not in set_2:   # If elem not in set_2, add to result
             result.add(elements)
 
-    for elements in set_2:
-        if elements not in set_1:
+    for elements in set_2:  # Loop through elements in set_2
+        if elements not in set_1:   # If elem not in set_1, add to result
             result.add(elements)
-    return result
+    return result   # Return the result set
