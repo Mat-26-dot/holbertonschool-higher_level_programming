f91707d3edad4fbdd923c7e093896855280606c1
md <md@outlook.com>
2025-05-20 21:29:59 +1000

    9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
 
-    new_dict = {}
-    for key in a_dictionary: # iterate through original dict
-        value = a_dictionary[key]  # multiply each value by 2 and add to the new dictionary
-        new_value = value *2
-        new_dict[key] = new_value 
+    new_dict = {}   # created new value to store empty list
+    for key in a_dictionary:    # iterate through original
+        value = a_dictionary[key]   # multiply each value by
+        new_dict[key] = value 
     return new_dict
