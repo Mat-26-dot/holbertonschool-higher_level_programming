7f1e29be91f49a8040e8829a1cf3612299d90b33
md <md@outlook.com>
2025-05-21 22:17:28 +1000

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except: (IndexError, ValueError)
-        break
+                count += 1
+        except (IndexError, ValueError):
+            break
     print()
     return count
