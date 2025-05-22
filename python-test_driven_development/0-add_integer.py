334d20e217bdf3b65fcda4fbdc7879be408c33d5
md <md@outlook.com>
2025-05-22 15:46:35 +1000

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,7 +16,7 @@ def add_integer(a, b=98):
         sum of a and b.
     """
 
-    if type(a) not in (int, float):
+    if not isinstance(int, float):
         raise TypeError("a must be an integer")
     elif type(b) not in (int, float):
         raise TypeError("b must be an integer")
