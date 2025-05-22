3c32fd4b237ac41e3af3e6d544abe5e212e33dbc
md <md@outlook.com>
2025-05-22 10:19:10 +1000

    3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,8 +5,6 @@ def safe_print_division(a, b):
         result = a / b
         return result
     except: ZeroDivisionError
-
     finally:
-            print("Inside result: {}".format(result))
+        print("Inside result: {}".format(result))
     return result
-   
