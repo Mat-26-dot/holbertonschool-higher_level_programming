2f3860153d788e492e14fedb0019a737edc11331
md <md@outlook.com>
2025-05-22 16:13:42 +1000

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,8 +6,8 @@ def add_integer(a, b=98):
     """Adds two integers.
 
     Args:
-        a: first int.
-        b: second int, default value is 98.
+        a: first int or float.
+        b: second int or float, default value is 98.
 
     Raises:
         TypeError: if a, b are neither int nor float.
@@ -22,6 +22,3 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/0-add_integer.txt")
