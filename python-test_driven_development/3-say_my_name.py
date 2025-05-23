d513ab24a789eb7268a6df88a3d902f06128e880
md <md@outlook.com>
2025-05-23 11:29:32 +1000

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,7 +6,6 @@ def say_my_name(first_name, last_name=""):
     """string argument validation"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance (last_name, str):
+    elif not isinstance (last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
-    print("My name is {}".format(first_name))
