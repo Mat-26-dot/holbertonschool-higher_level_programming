d1bd72a782bb774326eb79814d1bf382f364c34c
md <md@outlook.com>
2025-05-27 20:08:02 +1000

    3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Defines a rectangle"""
 
-    def __init__(self, width=0, height =0):
+    def __init__(self, width=0, height=0):
         """Initialize a new rectangle instance
         Args:
             width (int): The width of the rectangle (default 0)
