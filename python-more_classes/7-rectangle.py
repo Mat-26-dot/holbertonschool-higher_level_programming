5520a1ec2294ad6d909f56335cd39ef22b396618
md <md@outlook.com>
2025-05-28 09:23:11 +1000

    7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -6,18 +6,18 @@ class Rectangle:
     """Defines a rectangle"""
     number_of_instances = 0
     print_symbol = "#"
-    
+
     def __init__(self, width=0, height=0):
         self.width = width  # Initialised for setter & getter
         self.height = height  # Initialised for setter & getter
         number_of_instances = 0
         print_symbol = "#"
         Rectangle.number_of_instances += 1
-        
+
     @property
     def width(self):
             return self.__width # retrieving width
-        
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -25,7 +25,7 @@ class Rectangle:
         if value < 0:
                 raise ValueError("width must be >= 0")
         self.__width = value
-        
+
     @property
     def height(self):
         return self.__height # retrieve height
@@ -40,7 +40,7 @@ class Rectangle:
 
     def area(self):
         return self.width * self.height
-        
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0    # handling edge cases if perimeter 0
@@ -49,14 +49,13 @@ class Rectangle:
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ""   # handling an empty string (edge cases)
-        
+
         row = str(self.print_symbol) * self.width
         return "\n".join([row for _ in range(self.height)])
-    
+
     def __repr__(self):
             return f"Rectangle({self.width}, {self.height})"
-        
+
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-        
