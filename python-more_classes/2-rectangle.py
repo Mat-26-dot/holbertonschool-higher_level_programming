eb6e43e29b741f2a18b169daf525b09102f110f0
md <md@outlook.com>
2025-05-27 16:15:36 +1000

    2-rectangle.py

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a rectangle by its height and width"""
+
+
+def __init__(self, width=0, height=0):
+    self.width = width # Calls the width setter
+    self.height = height # Calls the height setter
+
+    @property
+    def width(self):
+        """Get the width of a rectangle"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    @property
+    def height(self):
+        """Get the height of a rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return area of a rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Return the perimeter of the recytangle
+        if width and height is 0, returns 0"""
+
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
