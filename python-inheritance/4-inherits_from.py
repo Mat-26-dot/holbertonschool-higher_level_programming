d3ef58f73fa05a4d623ade5328d2d984765157a0
md <md@outlook.com>
2025-05-28 17:46:29 +1000

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-
+"""object is an instance of a class that inherited"""
 
 
 def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a class that inherited
+    from a_class; otherwise False.
+    """ 
     return issubclass(type(obj), a_class) and type(obj) != a_class
