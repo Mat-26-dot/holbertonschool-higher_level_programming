9f617d7f2e274a8c743e28ee91194651fbe65cb6
md <md@outlook.com>
2025-05-28 16:46:10 +1000

    2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the same class"""
 
+is_same_class = __import__('2-is_same_class').is_same_class
 def is_sameclass(obj, a_class):
-    """Same class containing obj, & a_class"""
+    """Same class containing obj, & a_class""" 
     return type(obj) is a_class
