9bd2d08e3b41108caeb8ab148eb43f3979040f75
md <md@outlook.com>
2025-05-28 17:10:26 +1000

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Check if object is instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if args (obj, a_class) are the same class"""
+    return isinstance(obj, a_class)
