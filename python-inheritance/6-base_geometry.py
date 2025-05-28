8cf0e22447309ce6217c42d2c159c657e5be8040
md <md@outlook.com>
2025-05-28 21:56:00 +1000

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class Defined"""
+
+
+class BaseGeometry:
+    """Class defined"""
+    def area(self):
+        """method for calculated area"""
+        raise Exception("area() is not implemented")
