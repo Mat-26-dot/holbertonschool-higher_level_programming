c7a3a3e83af4a91a2a0095161c22768007e70d9d
md <md@outlook.com>
2025-05-31 16:18:10 +1000

    10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,6 +6,7 @@ Rectangle = __import__('9-rectangle.py').Rectangle
 class square(Rectangle):
     """Represents a Square"""
     def __init__(self, size):
+        """Initializes a Rectangle with validated width"""   
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
