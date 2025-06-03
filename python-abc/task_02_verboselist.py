9e7483b5383f7de8152eeb33aec5ac9d560a26f8
md <md@outlook.com>
2025-06-03 15:10:37 +1000

    task_02_verboselist.py

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from abc import ABC
+
+class VerboseList(list):
+
+    def append(self, item):
+        print(f"Added {item} to the list")
+        super().append(item)
+    
+    def extend(self, items):
+        print(f"Extended the list with {len(items)} items: {items}")
+        super().extend(items)
+    
+    def remove(self, item):
+        print(f"Removed {item} from list")
+        super().remove(item)    
+    
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped {item} from the list")
+        super().pop(index)
