8050fe8e1d20ce97f88e893123cf4da02f384c89
md <md@outlook.com>
2025-06-04 15:01:33 +1000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
+    # function reads a text file (UTF8) and prints to stdout
 def read_file(filename=""):
-
+    # Reads a txt file
     with open(filename, encoding="UTF-8") as f:
         print(f.read())
-
