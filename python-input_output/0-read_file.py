1a4380c8386d9843123e415a50985c29c15122dd
md <md@outlook.com>
2025-06-04 15:11:19 +1000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """
-This module contains a function that reads a UTF-8 text file 
+This module contains a function that reads a UTF-8 text file
 and prints its contents to standard output.
-""" 
+"""
 def read_file(filename=""):
+    
+    
     """function reads a text file (UTF8) and prints to stdout"""
     with open(filename, encoding="UTF-8") as f:
         print(f.read(), end="")
