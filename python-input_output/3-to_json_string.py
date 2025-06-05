35581e87be9130256bc0f3136ed8545be1f09e57
md <md@outlook.com>
2025-06-05 10:42:41 +1000

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+"""returns the JSON representation of an object (string):"""
 
 
 import json
 
 def to_json_string(my_obj):
-
+    """
+    Converts a Python object to its JSON string representation.
+    """
     return (my_obj)
