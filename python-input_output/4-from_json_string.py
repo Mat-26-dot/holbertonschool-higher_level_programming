a18d75c714e3dbeafa933db061f7b5b678797f9a
md <md@outlook.com>
2025-06-05 11:46:27 +1000

    4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """A module that returns an object (Python data structure)
     represented by a JSON string"""
+
+
 import json
 
 def from_json_string(my_str):
     """function that returns an object (Python data structure) 
     represented by a JSON string"""
-    return json.load, my_str
+    return json.loads(my_str)
