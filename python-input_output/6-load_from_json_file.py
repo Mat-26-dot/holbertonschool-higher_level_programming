ba2a642b62dad476e91b20a9fc7ac992152def79
md <md@outlook.com>
2025-06-06 11:58:21 +1000

    6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """This module provides utilities for 
 reading and writing JSON files."""
+
+
 import json
 
+
 def load_from_json_file(filename):
-    """Loads and parses JSON data from a file."""
     try:
         with open(filename, 'r') as f:
             return json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []
+    except json.JSONDecodeError as e:
+        print(f"[JSONDecodeError] {e}")
+    except FileNotFoundError as e:
+        print(f"[FileNotFoundError] {e}")
+    except PermissionError as e:
+        print(f"[PermissionError] {e}")
+
 
 
 
