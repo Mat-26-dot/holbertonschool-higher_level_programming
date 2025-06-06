ec2803bca948651e50a4c7bcfa5e0506d61803bf
md <md@outlook.com>
2025-06-06 16:27:47 +1000

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,9 +4,11 @@ and then save them to a file"""
 
 
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
+"""Correctly import the functions from your modules"""
 filename = "add_item.json"
 
 try:
