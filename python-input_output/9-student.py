c0639ae0ec90b0ccd84ca5a8c5a2fde236db6044
md <md@outlook.com>
2025-06-09 12:58:28 +1000

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """Module that defines student by dict representation"""
+
+
+
 class Student:
     """Class that defines a student attributes"""
     def __init__(self, first_name, last_name, age):
+        
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
     def to_json(self):
         student_dict = {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age
-
-    }
+                        "first_name": self.first_name,
+                        "last_name": self.last_name,
+                        "age": self.age
+                    }
         return student_dict
