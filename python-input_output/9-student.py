2d3f85adac5c58f06862378baf1ec053e94b6fd9
md <md@outlook.com>
2025-06-09 12:44:25 +1000

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
+"""Module that defines student by dict representation"""
 class Student:
-
+    """Class that defines a student attributes"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
