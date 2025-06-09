b54b54b02d49ac55f6f0434c3cf269a07d964d5c
md <md@outlook.com>
2025-06-09 15:12:24 +1000

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module that defines student by dict representation"""
+
+
 class Student:
     """Class that defines a student attributes"""
     def __init__(self, first_name, last_name, age):
@@ -8,10 +10,10 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        student_dict = self.__dict__.copy()
+        student_dict = self.__dict__.copy ()
         if isinstance(attrs, list) and all (isinstance(attr, str)
-        for attr in attrs):
+            for attr in attrs):
             student_dict = {key: student_dict[key] for key in attrs if key
         in student_dict}
-            
+
         return student_dict
