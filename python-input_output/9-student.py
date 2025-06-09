8a626ba6025850a8aa2ef5e7e3897f67e527d412
md <md@outlook.com>
2025-06-09 12:47:38 +1000

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,10 +6,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age 
-        print("Initializing Student:")
-        print("First name:", first_name)
-        print("Last name:", last_name)
-        print("Age:", age)
     def to_json(self): 
         student_dict = {
             "first_name": self.first_name,
