51d61d13768d3283b7840f3746b0fb6fdff220b0
md <md@outlook.com>
2025-06-10 13:51:00 +1000

    12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            previous_row = triangle[i - 1]
+            for j in range(1, i):
+                row.append(previous_row[j - 1] + previous_row[j])
+            row.append(1)
+        triangle.append(row)
+    return triangle
+
