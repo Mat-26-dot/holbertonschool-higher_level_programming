7dbaa160bcb6bbfc2fbc71c555f6cc70f49b94e4
md <md@outlook.com>
2025-06-10 12:13:02 +1000

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -4,8 +4,7 @@
 
 class Student:
     """A class that defines a student"""
-    
-    
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
