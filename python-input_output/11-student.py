4e854e95439e7bdfcc4e05033fc5d4cff56b3c91
md <md@outlook.com>
2025-06-10 11:20:56 +1000

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-class Student:
+"""A module that defines a student"""
 
+
+class Student:
+    
+    
+    """A module that defines a student"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
