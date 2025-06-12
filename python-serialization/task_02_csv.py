17ed403ccf1089bde034ebeab47cfdbbddb23e4a
md <md@outlook.com>
2025-06-12 11:30:33 +1000

    task_02_csv.py

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+ 
+import csv 
+import json
+
+def convert_csv_to_json(csv_filename):
+
+    try:
+        with open(csv_filename, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = list(reader)
+        
+        with open('data.json', 'w') as json_file:
+            json.dump(data, json_file)
+    except Exception:
+        return True
