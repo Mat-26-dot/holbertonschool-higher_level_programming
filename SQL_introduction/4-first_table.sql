b7f3556a794017ca706b16db1702c39f77747527
md <md@outlook.com>
2025-06-30 14:00:14 +1000

    4-first_table.sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,13 +1,7 @@
 -- creates a table called first_table in the current database
 
-USE hbtn_test_db_4;
-
 CREATE TABLE IF NOT EXISTS first_table (
 
-    id INT
+    id INT,
     name VARCHAR(256)
-
-INSERT INTO first_table (id, name) VALUES (1, 'Alice'), (2, 'Bob');
-SELECT COUNT(id) FROM first_table;
 );
-
