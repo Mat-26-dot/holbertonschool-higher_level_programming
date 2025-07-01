5f3570239a1aa3cedf0369e5a3d21e6b597ea8fd
md <md@outlook.com>
2025-07-01 15:58:21 +1000

    15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- lists the number of records with the same score in the table
 
-SELECT score
+SELECT score, COUNT(*) AS number
 FROM second_table
-WHERE score = value
-ORDER BY score DESC;
+GROUP BY score
+ORDER BY number DESC;
