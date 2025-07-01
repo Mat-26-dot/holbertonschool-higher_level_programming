a8f1ea61bfcb3376f2baa0b3e18a35046b240272
md <md@outlook.com>
2025-07-01 11:10:48 +1000

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS second_table; (
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table VALUES (
-
+INSERT INTO second_table VALUES (id, name, score)
+(
     (1, "John", 10)
     (2, "Alex", 3)
     (3, "Bob", 14)
