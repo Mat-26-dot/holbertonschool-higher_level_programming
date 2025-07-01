aca5c552f4a1315e7d739c078a237da78082864b
md <md@outlook.com>
2025-07-01 11:23:12 +1000

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,15 +1,14 @@
 -- create table in database server adding multiple rows
 
-CREATE TABLE IF NOT EXISTS second_table; (
+CREATE TABLE IF NOT EXISTS second_table (
 
-    id INT
+    id INT,
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table VALUES (id, name, score)
-(
-    (1, "John", 10)
-    (2, "Alex", 3)
-    (3, "Bob", 14)
-    (4, "George", 8)
-);
+INSERT INTO second_table (id, name, score) VALUES
+
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8)
