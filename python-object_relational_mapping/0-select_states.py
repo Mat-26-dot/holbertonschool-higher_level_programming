1338cbe3d1471110b0eb9a3cf9ff42f5c19c64f5
md <md@outlook.com>
2025-07-03 14:20:43 +1000

    0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,8 +8,8 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-   
-    conn = MySQLdb.connect( 
+
+    conn = MySQLdb.connect(
         host='localhost',
         port=3306,
         user=sys.argv[1],
