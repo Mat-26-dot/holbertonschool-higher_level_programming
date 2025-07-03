e286e3984e50bda8ce195c7a33777694bcab8426
md <md@outlook.com>
2025-07-03 16:10:44 +1000

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-lists all states with a name starting with N (upper N) from the database 
+lists all states with a name starting with N (upper N) from the database
 """
 
 import MySQLdb
@@ -16,7 +16,8 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' "
+    "ORDER BY states.id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
