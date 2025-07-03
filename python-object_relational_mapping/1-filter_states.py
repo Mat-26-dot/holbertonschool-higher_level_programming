2f38a7a6f33003a22e9ecf4cd705d17548680fa9
md <md@outlook.com>
2025-07-03 16:13:27 +1000

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     )
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' "
-                "ORDER BY states.id ASC")
+                   "ORDER BY states.id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
