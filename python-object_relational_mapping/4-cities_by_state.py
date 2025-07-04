bd1f0b7b59d202b5fcfc95d6c03e47dff8924199
md <md@outlook.com>
2025-07-04 15:22:15 +1000

    4-cities_by_state.py

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,8 +9,8 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> <database name>" 
-            .format(sys.argv[0]))
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     # Connect to the MySQL database #
