365ece841bc8bfae236bf50378a3b77b065f9ee4
md <md@outlook.com>
2025-07-07 10:29:12 +1000

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+This module takes an argument and displays all values in the states table
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Initialise Args #
+    state_name = sys.argv[4]
+    # Connect to the MySQL database #
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+    # Create a cursor and execute the query #
+    cursor = conn.cursor()
+    query = ("""SELECT cities.id, cities.name, states.name
+             FROM cities
+             JOIN states ON cities.state_id = states.id
+             ORDER BY cities.id ASC""")
+    # Query is parsed separately to the database to prevent name tampering
+    # such as SQL injection #
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
