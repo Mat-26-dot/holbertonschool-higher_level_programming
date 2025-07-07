33c3358a57ff6b00f98284535346473736faaf75
md <md@outlook.com>
2025-07-07 11:27:12 +1000

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -22,12 +22,10 @@ if __name__ == "__main__":
     )
     # Create a cursor and execute the query #
     cursor = conn.cursor()
-    query = ("INSERT INTO cities (name, state.id)"
-             "VALUES (%s, %s)"
-             "ORDER BY cities.id ASC")
+    query = "(INSERT INTO cities (name, state.id) VALUES %s, %s)"
     # Query is parsed separately to the database to prevent name tampering
     # such as SQL injection #
-    cursor.execute(query,)
+    cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
         print(row)
