b6ba7b522712f64a8b2026db347f587ef83a06e6
md <md@outlook.com>
2025-07-10 13:06:03 +1000

    8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,34 +14,30 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: script.py <username> <password> <database>")
         sys.exit(1)
-    # Parse command-line args    
+    # Parse command-line args
     username=sys.argv[1]
     password=sys.argv[2]
     db_name=sys.argv[3]
-    
+
     # Makes connection with db to communicate with db using SQLAlchemy
     # mysql+mysqldb - connect mysqldb with mysql driver
     engine = create_engine(
         f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
         pool_pre_ping=True
     )
-    # Why this section is needed - lets you interact with db using python 
+    # Why this section is needed - lets you interact with db using python
     # objects, not using raw SQL
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # This query will retrieve the first state in the db in 
+    # This query will retrieve the first state in the db in
     # the order of State.id = .first(id)
     first_state = session.query(State).order_by(State.id).first()
 
-    # Display the result 
+    # Display the result
     if first_state:
         print(f"{first_state.id}: {first_state.name}")
     else:
         print("Nothing")
 
     session.close()
-
-
-
-
