6a84eae123273bcfd4f7980793600c16ad4a3b8f
Mathijs Dickson <md@outlook.com>
2025-07-29 17:22:10 +1000

    7-multi_c.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -5,3 +5,12 @@ for (let i = 0; i < lines.length; i++) {
   output += lines[i] + (i === lines.length - 1 ? '' : '\n');
 }
 console.log(output);
+
+Explanation
+// We store all lines in an array.
+
+// We loop over the array and build a string with each line.
+
+// We add a newline after each line except the last one (using ternary operator).
+
+//We print all lines at once using a single console.log.
