19fb015c3e6e83ff6bc97572a85cf7e21f48261b
Mathijs Dickson <md@outlook.com>
2025-07-29 17:35:25 +1000

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,8 +2,8 @@
 const arg = process.argv[2];
 const x = parseInt(arg, 10);
 if (isNaN(x)) {
-    console.log('Missing number of occurences');
-} else {
+console.log('Missing number of occurences');
+} else if (x > 0) {
 const arr = [];
 for (let i = 0; i < x; i++) {
   arr.push('C is fun');
