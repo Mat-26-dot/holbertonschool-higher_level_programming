eb552e2898b3f7ee6e0166e34a827b2d14797eaa
Mathijs Dickson <md@outlook.com>
2025-07-29 14:34:36 +1000

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let output = '';
 for (let i = 0; i < lines.length; i++) {
-    output += lines[i] + (i === lines.length - 1 ? '' : '\n');
+  output += lines[i] + (i === lines.length - 1 ? '' : '\n');
 }
 console.log(output);
