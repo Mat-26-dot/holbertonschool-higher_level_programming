3295a70e73585a2637ee94ed667febef88611ba6
Mathijs Dickson <md@outlook.com>
2025-07-30 14:06:34 +1000

    9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 function add(a, b) {
+  console.log(a + b);
 } 
-    add = parseInt(process.argv[2, 3]);
-    console.log(add);
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+
+add(a, b);
+
 
