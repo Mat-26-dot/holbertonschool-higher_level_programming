690497a5fd0b37388cff59e7d394a38e85949831
Mathijs Dickson <md@outlook.com>
2025-07-30 11:49:34 +1000

    8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,12 +5,12 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-  console.log('X'.repeat(size));
-    }
+    console.log('X'.repeat(size));
+  }
 }
-//The script gets the size from the command line.
+// The script gets the size from the command line.
 
-//If no valid size is provided, prints an error. (Missing size)
+// If no valid size is provided, prints an error. (Missing size)
 
-//Otherwise, prints a square of X's, using a for-loop,
+// Otherwise, prints a square of X's, using a for-loop,
 // with sides equal to the given size.
