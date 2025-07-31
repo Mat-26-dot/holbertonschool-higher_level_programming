dd92251ef3e6faf68fb5d39419ca48c05b94c629
Mathijs Dickson <md@outlook.com>
2025-07-31 13:22:16 +1000

    11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,22 +4,22 @@ const numbers = args.map(arg => Number(arg));
 
 if (numbers.length <= 1) {
   console.log(0);
-  }
+}
 // Initialize max and secondmax
 let max = -Infinity;
 let secondMax = -Infinity;
 // Iterate through numbers to find max and secondMax
 for (const num of numbers) {
   if (num > max) {
-  secondMax = max;
+    secondMax = max;
     max = num;
-} else if (num < max && num > secondMax) {
-  secondMax = num;
-    }
+  } else if (num < max && num > secondMax) {
+    secondMax = num;
+  }
   }
 // After iteration, print secondMax or 0 if it doesn't exist
-  if (secondMax === -Infinity) {
-    console.log(0);
-  } else {
-    console.log(secondMax);
-  }
+if (secondMax === -Infinity) {
+  console.log(0);
+} else {
+  console.log(secondMax);
+}
