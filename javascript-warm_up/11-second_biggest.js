5cdefa1688823d2af2bbb8fbfcd5e94b97421a0e
Mathijs Dickson <md@outlook.com>
2025-07-31 13:19:57 +1000

    11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 const numbers = args.map(arg => Number(arg));
-  
+
 if (numbers.length <= 1) {
-    console.log(0);
+  console.log(0);
   }
-  // Initialize max and secondmax
-  let max = -Infinity;
-  let secondMax = -Infinity;
+// Initialize max and secondmax
+let max = -Infinity;
+let secondMax = -Infinity;
 // Iterate through numbers to find max and secondMax
-  for (const num of numbers) {
-    if (num > max) {
-        secondMax = max;
+for (const num of numbers) {
+  if (num > max) {
+  secondMax = max;
     max = num;
-    } else if (num < max && num > secondMax) {
-        secondMax = num;
+} else if (num < max && num > secondMax) {
+  secondMax = num;
     }
   }
 // After iteration, print secondMax or 0 if it doesn't exist
